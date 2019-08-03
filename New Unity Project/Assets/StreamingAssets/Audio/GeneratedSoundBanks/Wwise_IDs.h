572b4fe074074b690990299a3d4360c6027a2a93
kkxszz <kkxszz@gmail.com>
2019-08-03 10:05:49 +0100

    Updateeeeeeeeee

--- New Unity Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ New Unity Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,22 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID DOOR_01_CLOSE_PARTA = 1350431389U;
-        static const AkUniqueID DOOR_01_CLOSE_PARTB = 1350431390U;
-        static const AkUniqueID DOOR_01_CLOSE_PARTC = 1350431391U;
-        static const AkUniqueID DOOR_01_OPEN_PARTA = 255856401U;
-        static const AkUniqueID DOOR_01_OPEN_PARTB = 255856402U;
+        static const AkUniqueID DOOR_NARROW_AXIS_CLOSE = 2407747538U;
+        static const AkUniqueID DOOR_NARROW_AXIS_OPEN = 2916866438U;
+        static const AkUniqueID DOOR_NARROW_HANDLE_CLOSE = 501750955U;
+        static const AkUniqueID DOOR_NARROW_HANDLE_OPEN = 982290157U;
+        static const AkUniqueID DOOR_NARROW_KEYSOUND = 3041433330U;
+        static const AkUniqueID FIREPLACE = 1404152342U;
+        static const AkUniqueID GRAMOPHONE_CHARGE = 2879668892U;
+        static const AkUniqueID GRAMOPHONE_MAINGHOSTY = 2232826297U;
+        static const AkUniqueID GRAMOPHONE_MAINNOISE = 2673340027U;
+        static const AkUniqueID GRAMOPHONE_NEEDLETAP = 3525613408U;
+        static const AkUniqueID GRAMOPHONE_ONOFFTAP = 435953925U;
+        static const AkUniqueID GRAMOPHONE_SQUEAK_1 = 850197958U;
+        static const AkUniqueID GRAMOPHONE_SQUEAK_2 = 850197957U;
+        static const AkUniqueID GRAMOPHONE_SQUEAK_3 = 850197956U;
+        static const AkUniqueID LIGHTSWITCH_OFF = 280670697U;
+        static const AkUniqueID LIGHTSWITCH_ON = 2253880733U;
         static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
         static const AkUniqueID PLAYER_RAIN = 990547651U;
         static const AkUniqueID TV_01 = 1652545419U;
@@ -58,8 +69,8 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID AUDIOSOURCEDISTANCE = 989232327U;
-        static const AkUniqueID AUDIOSOURCEDISTANCE_SHORT = 3250513656U;
+        static const AkUniqueID DISTANCE_MAX10 = 1192632456U;
+        static const AkUniqueID DISTANCE_MAX20 = 1242965313U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -72,7 +83,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID DIEGETIC_MUSIC = 1515175539U;
-        static const AkUniqueID DIEGETIC_SFX = 1758314175U;
+        static const AkUniqueID DIEGETIC_SFX_MUSIC = 752329608U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID PLAYER = 1069431850U;
     } // namespace BUSSES
