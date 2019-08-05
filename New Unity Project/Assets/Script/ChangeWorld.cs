248b13bfb187a8cafe399980e7efa8078d9b1dc5
kkxszz <kkxszz@gmail.com>
2019-08-05 03:52:44 +0100

    Add a script to test WorldB sound
    挂在MainCamera上了

---
+++ New Unity Project/Assets/Script/ChangeWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWorld : MonoBehaviour {   
+    public int WorldNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (WorldNumber == 0)
+        {
+            AkSoundEngine.SetState("WorldState", "A");
+        }
+        else
+        {
+            AkSoundEngine.SetState("WorldState", "B");
+        }
+
+	}
+}
