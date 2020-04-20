7cfe70c49aa54e31e52dc4588a034b1dc0c0a1ee
szz1031 <kkxszz@gmail.com>
2020-04-20 18:32:45 +0800

    test1

--- New Unity Project/Assets/InteractStoryTV1.cs
+++ New Unity Project/Assets/InteractStoryTV1.cs
@@ -5,12 +5,13 @@ using GameSetting;
 public class InteractStoryTV1 : InteractStorySpecial<InteractStoryTV1> {
     public string DialogTips;
     public string[] Dialogues;
+    public GameObject Target;
 
     protected override void OnStageStart(enum_Stage stage)
     {
         base.OnStageStart(stage);
         if (stage == enum_Stage.Stage4)
-            AudioManager.PostEvent("StopObject", this.gameObject);
+            AudioManager.PostEvent("StopObject", Target);
     }
     public override void TryInteract()
     {
@@ -19,14 +20,14 @@ public class InteractStoryTV1 : InteractStorySpecial<InteractStoryTV1> {
     protected virtual void OnDialogInteracted(int index)
     {
         index++;
-        AudioManager.PostEvent("TV_1_Music_"+index.ToString(),this.gameObject);
+        AudioManager.PostEvent("TV_1_Music_"+index.ToString(),Target);
         //UIManager.Instance.AddSubtitle("Playing Music" + index.ToString());
         UIManager.Instance.AddTips("Music Switched");
         base.TryInteract();
     }
     public void RemoteInteract()
     {
-        AudioManager.PostEvent("TV_1_SetVolume", this.gameObject);
+        AudioManager.PostEvent("TV_1_SetVolume", Target);
         UIManager.Instance.AddSubtitle("The Volume is turned up now");
     }
 }
